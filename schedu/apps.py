eb7dddd6a43048f040fa699c9ec1b48234205052
zhongyah <18615535108@163.com>
2018-08-11 10:57:49 +0800

    Revert "复制新的项目"
    This reverts commit 0b011d3fb867be532b583c02190b8891bdc18833.

--- schedu/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScheduConfig(AppConfig):
-    name = 'schedu'
--- schedu/models.py
+++
@@ -1,4 +0,0 @@
-
-from django.contrib.auth.models import Permission, User
-from django.db import models
-
